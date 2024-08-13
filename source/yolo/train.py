343446848d33c7e7db17a17dbb68533852f4cd44
prox-kousei <k-osawa@prox-industries.jp>
2024-08-13 20:24:00 +0900

    reload chache to ignore dastaset

--- source/yolo/train.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO(
-    "yolov8n.pt"
-)  # yolov8n/s/m/l/xのいずれかを指定。多クラスの検出であるほど大きいパラメータが必要
-model.train(
-    data="./data.yaml", epochs=300, batch=20
-)  # 先ほど作成したデータセット内のyamlファイルまでのパスを指定
